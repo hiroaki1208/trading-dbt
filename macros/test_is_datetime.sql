c4fc4193d668e28302611a9f6c4361f3e6a58c74
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-14 17:22:38 +0900

    fix is_datetime test

--- macros/test_is_datetime.sql
+++ macros/test_is_datetime.sql
@@ -2,10 +2,7 @@
 
   SELECT COUNT(*)
   FROM {{ model }}
-  WHERE {{ column_name }} IS NOT NULL
-    AND (
-      SAFE.PARSE_DATETIME('%Y-%m-%d %H:%M:%S', CAST({{ column_name }} AS STRING)) IS NULL
-      AND SAFE.PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S', CAST({{ column_name }} AS STRING)) IS NULL
-    )
+  WHERE SAFE_CAST({{ column_name }} AS DATETIME) IS NULL
+    AND {{ column_name }} IS NOT NULL
 
 {% endtest %}
