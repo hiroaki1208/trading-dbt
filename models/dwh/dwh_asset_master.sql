700b93835057780793ff253c10b37dbd2553e549
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-15 12:37:32 +0900

    feat add dwh_asset_master

---
+++ models/dwh/dwh_asset_master.sql
@@ -0,0 +1,38 @@
+{{ config(materialized='view') }}
+
+-- SELECT
+--   PARSE_DATETIME('%Y-%m-%d %H:%M:%S', event_time_jst_str) AS event_time_jst,
+--   ticker,
+--   is_monitor,
+--   is_watch
+-- FROM
+--   `trading-prod-468212.trading.raw_asset_status_history`
+-- WHERE
+--   event_time_jst_str IS NOT NULL
+
+
+WITH
+
+add_rn AS (
+  SELECT
+    event_time_jst,
+    ticker,
+    is_monitor,
+    is_watch,
+    ROW_NUMBER() OVER (PARTITION BY ticker ORDER BY event_time_jst DESC) AS rn
+  FROM
+    {{ ref('dwh_asset_status_history') }}
+)
+
+SELECT
+  t0.ticker,
+  t1.asset_name,
+  t1.asset_type,
+  t0.is_monitor,
+  t0.is_watch,
+  t0.event_time_jst AS latest_status_chg_time_jst
+FROM
+  add_rn t0
+  LEFT JOIN {{ ref('stg_asset_name_master') }} t1 ON t0.ticker = t1.ticker
+WHERE
+  rn = 1
