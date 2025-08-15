aee5e8343b30b2aeee5b042311534736a26404aa
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-15 18:26:02 +0900

    fix declare variable

--- models/dm/dm_position.sql
+++ models/dm/dm_position.sql
@@ -10,12 +10,12 @@
     t1.asset_type,
     t1.asset_name,
     SUM(CASE WHEN buy_sell = 'buy' THEN order_count ELSE -1 * order_count END) AS position,
-    date_1day_ago AS partition_date
+    '{{ date_1day_ago }}' AS partition_date
   FROM
     {{ ref('dwh_trade_history_with_cash') }} t0
     left join {{ ref('dwh_asset_master') }} t1 using (ticker)
   WHERE
-    trade_date <= date_1day_ago
+    trade_date <= '{{ date_1day_ago }}'
     and ticker != 'cash' -- cash以外
   GROUP BY
     account,
