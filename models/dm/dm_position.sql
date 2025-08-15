b6af389d4016fca99530344541307b8080ea0c2c
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-15 18:14:36 +0900

    feat add dm_position

---
+++ models/dm/dm_position.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='incremental') }}
+
+{%- set _date_1day_ago = var('date_1day_ago', '9999-12-31') -%}
+
+-- 基準日時点でのポジション作成
+-- cashにポジションという考えはないのでこの時点で除外
+  SELECT
+    t0.account,
+    t0.ticker,
+    t1.asset_type,
+    t1.asset_name,
+    SUM(CASE WHEN buy_sell = 'buy' THEN order_count ELSE -1 * order_count END) AS position,
+    _date_1day_ago AS partition_date
+  FROM
+    {{ ref('dwh_trade_history_with_cash') }} t0
+    left join {{ ref('dwh_asset_master') }} t1 using (ticker)
+  WHERE
+    trade_date <= _date_1day_ago
+    and ticker != 'cash' -- cash以外
+  GROUP BY
+    account,
+    ticker,
+    asset_type,
+    asset_name
