2bffad89b982166e6e47924ab257c6a046f07fdd
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-08-26 17:00:52 +0900

    feat add avg price for position data

--- models/dm/dm_position.sql
+++ models/dm/dm_position.sql
@@ -1,27 +1,119 @@
+-- 各日付時点でのポジション作成
+-- 前提：ロングオンリー
 {{ config(
     materialized='incremental',
-    unique_key=['account', 'ticker', 'partition_date']
+    unique_key=['account', 'ticker', 'partition_date'],
+    partition_by={
+        "field": "partition_date",
+        "data_type": "date"
+    }
 ) }}
 
 {%- set date_1day_ago = var('date_1day_ago', '9999-12-31') -%}
 
--- 基準日時点でのポジション作成
--- cashにポジションという考えはないのでこの時点で除外
+WITH
+
+-- 全ての取引を1件ずつに分解
+explode_order AS (
+  SELECT
+    trade_date,
+    ticker,
+    trade_type,
+    account,
+    order_count,
+    price
+  FROM
+    {{ ref('stg_trade_history') }} t0
+    CROSS JOIN UNNEST(GENERATE_ARRAY(1, t0.order_count)) AS seq
+  WHERE
+    trade_date <= DATE('{{ date_1day_ago }}')
+    and ticker != 'cash' -- cash以外
+)
+
+-- buy,sellそれぞれに連番を付与
+, add_idx AS (
+  SELECT
+    trade_date,
+    ticker,
+    trade_type,
+    account,
+    order_count,
+    price,
+    ROW_NUMBER() OVER (PARTITION BY account, ticker, trade_type ORDER BY trade_date ASC) AS idx
+  FROM
+    explode_order
+)
+
+-- buy,sellを1件ずつ突き合わせるデータ
+, buy_sell_match_data AS (
   SELECT
     t0.account,
     t0.ticker,
-    t1.asset_type,
-    t1.asset_name,
-    SUM(CASE WHEN trade_type = 'buy' THEN order_count ELSE -1 * order_count END) AS position,
-    DATE('{{ date_1day_ago }}') AS partition_date
+    t0.trade_type AS trade_type_buy,
+    t0.order_count AS order_count_buy,
+    t0.price AS price_buy,
+    t0.idx AS idx_buy,
+    t1.trade_type AS trade_type_sell,
+    t1.order_count AS order_count_sell,
+    t1.price AS price_sell,
+    t1.idx AS idx_sell
   FROM
-    {{ ref('dwh_trade_history_with_cash') }} t0
-    left join {{ ref('dwh_asset_master') }} t1 using (ticker)
+    (SELECT * from add_idx where trade_type = 'buy') t0
+    LEFT JOIN (SELECT * from add_idx where trade_type = 'sell') t1 USING(account, ticker, idx)
+)
+
+-- ポジションと平均購入単価作成
+, position_data AS (
+  SELECT
+    t0.account,
+    t0.ticker,
+    SUM(t0.order_count_buy) AS position, -- 買いポジション量
+    AVG(t0.price_buy) AS avg_buy_price, -- 平均購入単価
+    -- DATE('{{ date_1day_ago }}') AS partition_date
+  FROM
+    buy_sell_match_data t0
+  WHERE
+    -- 買いに対して売りがないもののみ
+    -- 前提：ロングオンリー
+    idx_sell IS NULL
+  GROUP BY
+    account,
+    ticker
+  ORDER BY
+    account,
+    ticker
+)
+
+-- test用: ネットポジションが>=0なのかの確認
+, position_check AS (
+  SELECT
+    account,
+    ticker,
+    SUM(CASE WHEN trade_type = 'buy' THEN order_count ELSE -1 * order_count END) AS net_position
+  FROM
+    {{ ref('stg_trade_history') }}
   WHERE
     trade_date <= DATE('{{ date_1day_ago }}')
     and ticker != 'cash' -- cash以外
   GROUP BY
     account,
-    ticker,
-    asset_type,
-    asset_name
+    ticker
+)
+
+  SELECT
+    t0.account,
+    t0.ticker,
+    t0.position, -- 買いポジション量
+    t0.avg_buy_price, -- 平均購入単価
+    t1.net_position, -- test用: ネットポジションが>=0なのかの確認
+    DATE('{{ date_1day_ago }}') AS partition_date
+  FROM
+    position_data t0
+    LEFT JOIN position_check t1 USING(account, ticker)
+  ORDER BY
+    account,
+    ticker
+
+
+
+;
