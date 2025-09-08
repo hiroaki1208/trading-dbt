0c3c84ec895b7beca856cebfb01a60690381ed6e
ikamirin <hiroaki_ikami@yahoo.co.jp>
2025-09-08 10:59:08 +0900

    feat chg price add method

--- models/dm/dm_daily_all_term_pl.sql
+++ models/dm/dm_daily_all_term_pl.sql
@@ -36,23 +36,40 @@ all_trade_data AS (
   SELECT
     t0.account,
     t0.ticker,
-    t0.position * t1.price AS position_close_amount -- ポジション解消金額
+    t1.asset_type,
+    t1.asset_name,
+    t1.is_active_weekend,
+    t0.position,
+    if(t1.is_active_weekend, partition_date, {{ to_prev_weekday("partition_date") }}) AS price_date_for_close
   FROM
     {{ ref('dwh_daily_position') }} t0
-    LEFT JOIN (
-      SELECT *
-      FROM {{ ref('dwh_price_history') }}
-      WHERE base_date = DATE('{{ date_1day_ago }}') and ohlc_type = 'close'
-    ) t1 using (ticker)
+    LEFT JOIN {{ ref('ref_ticker_info') }} t1 ON t0.ticker = t1.ticker
   WHERE
     t0.partition_date = DATE('{{ date_1day_ago }}')
+)
+
+, add_price_data AS (
+  SELECT
+    t0.account,
+    t0.ticker,
+    t0.asset_type,
+    t0.asset_name,
+    t0.position,
+    t1.price AS close_price_for_position -- ポジション解消用の終値
+  FROM
+    position_close_data t0
+    LEFT JOIN (
+      SELECT *
+      FROM {{ ref('dwh_close_price_history') }}
+      WHERE base_date BETWEEN DATE_SUB(DATE('{{ date_1day_ago }}'), INTERVAL 6 DAY) AND DATE('{{ date_1day_ago }}')
+    ) t1 ON t0.ticker = t1.ticker AND t0.price_date_for_close = t1.base_date
 )
 
 SELECT
   t0.account,
   t0.ticker,
-  t2.asset_type,
-  t2.asset_name,
+  t1.asset_type,
+  t1.asset_name,
   t0.total_buy_amount,
   IFNULL(t0.sell_amount_already, 0) AS sell_amount_already,
   IFNULL(t1.position_close_amount, 0) AS position_close_amount,
@@ -63,4 +80,3 @@ SELECT
 FROM
   all_trade_data t0
   LEFT JOIN position_close_data t1 using (account, ticker)
-  LEFT JOIN {{ ref('dwh_asset_master') }} t2 using (ticker)
--- models/dm/dm_daily_position_value.sql
+++ models/dm/dm_daily_position_value.sql
@@ -11,11 +11,30 @@
 
 {%- set date_1day_ago = var('date_1day_ago', '9999-12-31') -%}
 
+WITH
+
+position_data_raw AS (
+  SELECT
+    t0.account,
+    t0.ticker,
+    t1.asset_type,
+    t1.asset_name,
+    t1.is_active_weekend,
+    if(t1.is_active_weekend, partition_date, {{ to_prev_weekday("partition_date") }}) AS price_date_for_close,
+    t0.position,
+    t0.avg_buy_price
+  FROM
+    {{ ref('dwh_daily_position') }} t0
+    LEFT JOIN {{ ref('ref_ticker_info') }} t1 ON t0.ticker = t1.ticker
+  WHERE
+    t0.partition_date = DATE('{{ date_1day_ago }}')
+)
+
 SELECT
   t0.account,
   t0.ticker,
-  t2.asset_type,
-  t2.asset_name,
+  t0.asset_type,
+  t0.asset_name,
   t0.position,
   t0.avg_buy_price,
   t1.price AS close_price, -- 基準日時点の価格
@@ -23,12 +42,9 @@ SELECT
   (t1.price - t0.avg_buy_price) / t0.avg_buy_price AS unrealized_return_ratio, -- 含み益率(%)
   DATE('{{ date_1day_ago }}') AS partition_date
 FROM
-  {{ ref('dwh_daily_position') }} t0
-  LEFT JOIN (
-    SELECT *
-    FROM {{ ref('dwh_price_history') }}
-    WHERE base_date = DATE('{{ date_1day_ago }}') and ohlc_type = 'close'
-  ) t1 using (ticker)
-  LEFT JOIN {{ ref('dwh_asset_master') }} t2 using (ticker)
-WHERE
-  t0.partition_date = DATE('{{ date_1day_ago }}')
+  position_data_raw t0
+    LEFT JOIN (
+      SELECT *
+      FROM {{ ref('dwh_close_price_history') }}
+      WHERE base_date BETWEEN DATE_SUB(DATE('{{ date_1day_ago }}'), INTERVAL 6 DAY) AND DATE('{{ date_1day_ago }}')
+    ) t1 ON t0.ticker = t1.ticker AND t0.price_date_for_close = t1.base_date
